6dbb6d19809537182a16cc3adf74047482d37a40
Jackson Singleton <jackson.singleton1@gmail.com>
2025-03-16 00:07:30 -0500

    add everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
+import { useState } from "react"
+import Editor from "./components/editor"
+
 function App() {
 
+  const update = (val: string) => {
+    console.log(val)
+  }
+
+  const [vim, setVim] = useState(false);
+
   return (
-    <main>
-    </main>
+    <>
+      <main className="flex flex-col justify-center items-center h-screen">
+        <header className="m-4 bg-primary px-6">
+          <h1 className="text-4xl text-base font-semibold">CODLE</h1>
+        </header>
+        <div className="w-1/2 h-96 rounded-2xl overflow-hidden border border-secondary p-8 shadow-xl">
+          <Editor
+            onChange={update}
+            vim={vim}
+          />
+        </div>
+        <div className="flex items-center justify-center gap-8 mt-4 w-1/2">
+          <button className="bg-accent text-base px-4 py-2 rounded-md cursor-pointer font-semibold text-xl hover:brightness-80"><i className="bi bi-play-fill"></i>Run</button>
+
+          <div className="flex items-center gap-2">
+            <i className="devicon-vim-plain text-4xl text-accent"></i>
+            <input type="checkbox" name="vim" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" checked={vim} onChange={() => setVim(!vim)} />
+          </div>
+        </div>
+      </main>
+    </>
   )
 }
 
---
+++ src/components/editor.tsx
@@ -0,0 +1,68 @@
+import { useState, useCallback } from "react"
+import CodeMirror from "@uiw/react-codemirror"
+import { python } from "@codemirror/lang-python"
+import { createTheme } from "@uiw/codemirror-themes"
+import { tags as t } from '@lezer/highlight';
+import { vim } from "@replit/codemirror-vim"
+
+
+const monokai = createTheme({
+  theme: 'dark',
+  settings: {
+    background: '#2d2a2e',
+    backgroundImage: '',
+    foreground: '#fcfcfa',
+    caret: '#ff6188',
+    selection: '#ab9df226',
+    selectionMatch: '#ab9df226',
+    lineHighlight: '#8a91991a',
+    gutterBackground: '#2d2a2e',
+    gutterForeground: '#93929366',
+    fontSize: '18px',
+    fontFamily: "IBM Plex Mono, monospace",
+  },
+  styles: [
+    { tag: t.comment, color: '#72707299' }, // dimmed3
+    { tag: t.variableName, color: '#78dce8' }, // accent5
+    { tag: [t.string, t.special(t.brace)], color: '#a9dc76' }, // accent4
+    { tag: t.number, color: '#fc9867' }, // accent2
+    { tag: t.bool, color: '#ff6188' }, // accent1
+    { tag: t.null, color: '#ff6188' }, // accent1
+    { tag: t.keyword, color: '#ab9df2' }, // accent6
+    { tag: t.operator, color: '#fc9867' }, // accent2
+    { tag: t.className, color: '#ffd866' }, // accent3
+    { tag: t.definition(t.typeName), color: '#ffd866' }, // accent3
+    { tag: t.typeName, color: '#ab9df2' }, // accent6
+    { tag: t.angleBracket, color: '#5b595c' }, // dimmed4
+    { tag: t.tagName, color: '#ab9df2' }, // accent6
+    { tag: t.attributeName, color: '#fc9867' }, // accent2
+  ],
+});
+
+
+interface Props {
+    onChange: (val: string) => void;
+    vim: boolean;
+}
+
+export default function Editor(props: Props) {
+    const [value, setValue] = useState('print("Hello World")')
+
+    const onChange = useCallback((val: string) => {
+        props.onChange(val);
+        setValue(val);
+    }, []);
+
+    const ext = props.vim ? [vim()] : [];
+
+    return (
+        <CodeMirror
+            value={value}
+            extensions={[python(), ...ext]}
+            onChange={onChange}
+            theme={monokai}
+            height="378px"
+            className="w-full h-full"
+        />
+    )
+}
